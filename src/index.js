21d54d707cc0d12020d2974dfe3a9c71b781b855
Katerina Zamiatina <eozamyatina@gmail.com>
2022-08-16 16:07:49 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let string = str.split("");
+
+  for (let i = 0; i < string.length; i += 1) {
+    for (let k = 0; k < bracketsConfig.length; k += 1) {
+      if (
+        string[i] === bracketsConfig[k][0] &&
+        string[i + 1] === bracketsConfig[k][1]
+      ) {
+        counter = i;
+        string.splice(counter, 2);
+        i = i - (counter + 1);
+      }
+    }
+    if (string.length === 0) {
+      return true;
+    }
+  }
+
+  return false;
+};
